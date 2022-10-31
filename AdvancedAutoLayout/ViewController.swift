0d8f97ef63cace918cf04177317349bcd5b8d17d
Mindy Douglas <mindy.douglas@icloud.com>
2022-10-31 07:35:48 -0500

    Initial Commit

---
+++ AdvancedAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvancedAutoLayout
+//
+//  Created by Mindy Douglas on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
